3e593d5b75dcca58eaaeda6a4cd5e0e4f866a3ec
BasudevBharatBhushan <bharatbhushan173@gmail.com>
2023-06-26 20:26:00 +0530

    basic styling added

---
+++ README.md
@@ -0,0 +1,7 @@
+## Things I learnt
+
+- Flex using Tailwind
+- Responsivenss using Tailwind
+- Dark Mode using Tailwind
+
+Reference - [developedbyed]('https://youtu.be/k-Pi5ZMxHWY')
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,16 @@ const colors = require("tailwindcss/colors");
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   mode: "jit", //Just in time compiler
-  darkMode: false, // or 'media' or 'class'
+  darkMode: "class", // or 'media' or 'class'
   content: ["./src/**/*.{html,js,jsx}"],
   theme: {
     extend: {
       colors: {
         primary: "#202225",
         secondary: "#5865f2",
+      },
+      fontFamily: {
+        burtons: "burtons",
       },
     },
   },
